6dcb7f5b39c02bebb2193778495183b28a447ac3
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-16 14:47:17 +0300

    Lesson_5. Task 7

--- lesson_5/src/App.js
+++ lesson_5/src/App.js
@@ -2,23 +2,26 @@ import {useState, useRef} from 'react'
 import { format } from 'date-fns'
 
 function App() {
-  const [summ, setSumm] = useState()
+  const [text, setText] = useState()
 
   const inputEL1 = useRef();
   const inputEL2 = useRef();
+  const inputEL3 = useRef();
 
   const handleClick = () => {
-    setSumm(Number(inputEL1.current.value) + Number(inputEL2.current.value));
+    setText([inputEL1.current.value, inputEL2.current.value, inputEL3.current.value].join(' '));
     inputEL1.current.value = '';
     inputEL2.current.value = '';
+    inputEL3.current.value = '';
   };
 
   return (
     <div>
-      <input ref={inputEL1} type='number'/>
-      <input ref={inputEL2} type='number'/>
-      <button onClick={handleClick}>Submit</button>
-      <p>{summ}</p>
+      <input ref={inputEL2} type='text' placeholder='Имя' />
+      <input ref={inputEL1} type='text' placeholder='Фамилия' />
+      <input ref={inputEL3} type='text' placeholder='Отчество' />
+      <button onClick={handleClick}>Нажми на меня</button>
+      <p>{text}</p>
     </div>
   );
 }
