bb2123ad3fbecc0925a7af425624d5c238d126dc
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-16 11:05:03 +0300

    Lesson_1. Task 10

--- lesson_1/src/App.js
+++ lesson_1/src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 
 function App() {
   const classes = useStyles();
-  const arr = ['a', 'b', 'c', 'd', 'e']
+  const getText = () => <p>текст</p>
 
   return (
     <div>
-      <ul>
-        {arr.map((value) => <li key={value}>{value}</li>)}
-      </ul>
+      {getText()}
     </div>
   );
 }
