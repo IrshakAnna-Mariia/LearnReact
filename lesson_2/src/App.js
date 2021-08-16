9451a7fd74431e3a4890fb05539be689ebd335dc
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-16 11:34:17 +0300

    Lesson_2. Task 2

--- lesson_2/src/App.js
+++ lesson_2/src/App.js
@@ -1,12 +1,13 @@
-import {useState} from 'react'
+import useStyles from './styles';
 
 function App() {
-  const [name, setName] = useState('Иван')
-  const [age, setAge] = useState(25)
+  const classes = useStyles();
+
+  const handleClick = () => alert('hello');
 
   return (
     <div>
-      имя: {name}, возраст: {age}
+      <button className={classes.button} onClick={handleClick}>нажми на меня</button>
     </div>
   );
 }
---
+++ lesson_2/src/styles.js
@@ -0,0 +1,8 @@
+import {createUseStyles} from 'react-jss'
+
+const useStyles = createUseStyles({
+  button: {
+  }
+})
+
+export default useStyles
