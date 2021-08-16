38f48edb23ca782d8d38a08edea49f0010a2f702
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-16 14:08:36 +0300

    Lesson_5. Task 3

--- lesson_5/src/App.js
+++ lesson_5/src/App.js
@@ -1,16 +1,24 @@
-import {useState} from 'react'
+import {useState, useRef} from 'react'
+import { format } from 'date-fns'
 
 function App() {
   const [text, setText] = useState('');
+  const [year, setYear] = useState()
+  const inputEL = useRef();
 
   const handleChange = (e) => {
-    setText(e.target.value.toUpperCase())
+    const enteredText = e.target.value;
+    const reg = /^[0-9]{0,3}$/
+    if (reg.test(enteredText)) {
+      setText(enteredText);
+      enteredText ? setYear(format(new Date(), 'yyyy') - enteredText) : setYear()
+    }
   }
 
   return (
     <div>
-      <p>{text}</p>
-      <input type='text' defaultValue={''} onChange={handleChange}/>
+      <p>{year}</p>
+      <input ref={inputEL} type='text' value={text} onChange={handleChange}/>
     </div>
   );
 }
