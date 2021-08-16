554fbd5930c8054b6d50bd8d075e433c9bdfd09b
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-16 12:16:46 +0300

    Lesson_3. Task  7

--- lesson_3/src/App.js
+++ lesson_3/src/App.js
@@ -1,12 +1,16 @@
 import {useState} from 'react';
 
 function App() {
-  const [names, setNames] = useState(['Коля', 'Вася', 'Петя']);
+  const [hrefs, setHrefs] = useState([
+    { href: '1.html', text: 'ссылка 1' },
+    { href: '2.html', text: 'ссылка 2' },
+    { href: '3.html', text: 'ссылка 3' },
+  ]);
 
   return (
     <div>
       <ul>
-        {names.map((item, index) => <li key={item}>{item} - {index + 1}</li>)}
+        {hrefs.map((item) => <li key={item}><a href={item.href}>{item.text}</a></li>)}
       </ul>
     </div>
   );
