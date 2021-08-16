54e48b9bbdbf4dce8ff50e5880aa858105448fcd
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-16 16:10:02 +0300

    Lesson_5. Task 12

--- lesson_5/src/App.js
+++ lesson_5/src/App.js
@@ -1,23 +1,53 @@
-import {useState, useRef} from 'react'
+import {useState, useRef} from 'react';
+import useStyles from './styles'
 
 function App() {
-  const [array, setArray] = useState(['React', 'CSS', 'JavaScript']);
+  const [items, setItems] = useState({
+    users: [
+      { name: 'Коля', age: '30' },
+      { name: 'Вася', age: '40' },
+      { name: 'Петя', age: '50' },
+    ],
+    newUser: { name: '', age: '' },
+  });
 
-  const numberInput = useRef();
+  const classes = useStyles();
 
-  const handleClickRemove = () => setArray(prevArray => [...prevArray.filter((item, index) => index !== Number(numberInput.current.value))]);
+  const handleChangeName = (e) => {
+    setItems(prevItems => ({
+      users: [...prevItems.users],
+      newUser: {name: e.target.value, age: prevItems.newUser.age}
+    }))
+  };
+  const handleChageAge = (e) => {
+    setItems(prevItems => ({
+      users: [...prevItems.users],
+      newUser: { name: prevItems.newUser.name, age: e.target.value }
+    }))
+  };
+
+  const handleClickAdd = () => {
+    items.newUser.name && items.newUser.age && setItems(prevItems => ({
+      users: [...prevItems.users, {name: prevItems.newUser.name, age: prevItems.newUser.age}],
+      newUser: { name: '', age: '' }
+    }))
+  };
 
   return (
     <div>
-      <ul>
-        {array.map((item) => (
-          <li key={item}>
-            {item}
-          </li>
+      <table>
+        <tbody>
+        {items.users.map((item) => (
+          <tr key={item.name}>
+            <td className={classes.td}>{item.name}</td>
+            <td className={classes.td}>{item.age}</td>
+          </tr>
         ))}
-      </ul>
-      <input ref={numberInput} type='number' />
-      <button onClick={handleClickRemove}>Удалить</button>
+        </tbody>
+      </table>
+      <input type='text' value={items.newUser.name} onChange={handleChangeName} placeholder='Имя' />
+      <input type='number' value={items.newUser.age} onChange={handleChageAge} placeholder='Возраст' />
+      <button onClick={handleClickAdd}>Добавить</button>
     </div>
   );
 }
---
+++ lesson_5/src/styles.js
@@ -0,0 +1,9 @@
+import {createUseStyles} from 'react-jss'
+
+const useStyles = createUseStyles({
+  td: {
+    border: '1px solid black'
+  }
+})
+
+export default useStyles;
