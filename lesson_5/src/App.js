1b368deccaf446c8f7fc203e7f929b67c29f2a03
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-16 15:31:00 +0300

    Lesson_5. Task 11

--- lesson_5/src/App.js
+++ lesson_5/src/App.js
@@ -1,34 +1,23 @@
 import {useState, useRef} from 'react'
 
 function App() {
-  const [hrefs, setHrefs] = useState([
-    { href: '1.html', text: 'ссылка 1' },
-    { href: '2.html', text: 'ссылка 2' },
-    { href: '3.html', text: 'ссылка 3' },
-    ]);
+  const [array, setArray] = useState(['React', 'CSS', 'JavaScript']);
 
-  const hrefInput = useRef();
-  const textInput = useRef();
+  const numberInput = useRef();
 
-  const handleClickAdd = () => {
-    setHrefs(prevHrefs => [...prevHrefs, {
-      href: hrefInput.current.value, 
-      text: textInput.current.value
-    }])
-  };
+  const handleClickRemove = () => setArray(prevArray => [...prevArray.filter((item, index) => index !== Number(numberInput.current.value))]);
 
   return (
     <div>
       <ul>
-        {hrefs.map((item) => (
-          <li key={item.href}>
-            <a href={item.href}>{item.text}</a>
+        {array.map((item) => (
+          <li key={item}>
+            {item}
           </li>
         ))}
       </ul>
-      <input ref={hrefInput} type='text' placeholder='Ссылка' />
-      <input ref={textInput} type='text' placeholder='Текст' />
-      <button onClick={handleClickAdd}>Добавить</button>
+      <input ref={numberInput} type='number' />
+      <button onClick={handleClickRemove}>Удалить</button>
     </div>
   );
 }
