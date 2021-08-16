a4a40471cf31acae8cf223be804e72d1e4e67844
Alex Kostenko <0984181760ss@gmail.com>
2021-08-16 10:13:25 +0300

    lesson_1. Task 1

---
+++ lesson_1/src/App.js
@@ -0,0 +1,15 @@
+import useStyles from './styles'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      текст
+    </div>
+  );
+}
+
+export default App;
