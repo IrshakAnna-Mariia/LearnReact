f5af21cfef3208d28ed2ab8ee5f199285b2e1c2f
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-16 14:30:30 +0300

    Lesson_5. Task 5

--- lesson_5/src/App.js
+++ lesson_5/src/App.js
@@ -2,25 +2,20 @@ import {useState, useRef} from 'react'
 import { format } from 'date-fns'
 
 function App() {
-  const [firesName, setFirstName] = useState()
-  const [lastName, setLastName] = useState()
-  const [patronymic, setPatronymic] = useState()
+  const [text, setText] = useState()
 
   const inputEL = useRef();
 
-  const handleChange = (e) => {
-    const enteredText = e.target.value.split(' ');
-    setLastName(enteredText[0]);
-    enteredText.length >= 2 && setFirstName(enteredText[1]);
-    enteredText.length >= 3 && setPatronymic(enteredText[2]);
-  }
+  const handleClick = () => {
+    setText(inputEL.current.value);
+    inputEL.current.value = '';
+  };
 
   return (
     <div>
-      <input ref={inputEL} type='text' onChange={handleChange}/>
-      <p>{lastName}</p>
-      <p>{firesName}</p>
-      <p>{patronymic}</p>
+      <input ref={inputEL} type='text'/>
+      <button onClick={handleClick}>Submit</button>
+      <p>{text}</p>
     </div>
   );
 }
