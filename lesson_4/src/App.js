c9ef20afefbd6ea5c7b5eb768c1efaa4e12cf1eb
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-16 12:26:15 +0300

    Lesson_4. Task 2

--- lesson_4/src/App.js
+++ lesson_4/src/App.js
@@ -3,10 +3,13 @@ import {useState} from 'react';
 function App() {
   const [names, setNames] = useState(['Коля', 'Вася', 'Петя', 'Иван', 'Дима'])
 
+  const handleClick = () => setNames((prevNames) => [...prevNames, 'пункт'])
+
   return (
     <div>
       <ul>
         {names.map((item) => <li>{item}</li>)}
+        <button onClick={handleClick}>Добавить</button>
       </ul>
     </div>
   );
