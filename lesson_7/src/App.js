93ef6af32ceb1867874b2a7aa4b8e0c7452d07eb
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-17 16:13:21 +0300

    Lesson_7. Task 8

--- lesson_7/src/App.js
+++ lesson_7/src/App.js
@@ -1,29 +1,49 @@
 import {useState} from 'react';
+import {format} from 'date-fns'
 
 function App() {
-  const [routes, setRoutes] = useState([
-    'Черкаси - Київ',
-    'Черкаси - Одеса',
-    'Черкасі - Лвів'
-  ]);
-  const [currentRoute, setCurrentRoute] = useState('');
+  const [notes, setNotes] = useState([
+    { text: 'First note', time: format(new Date(), "dd/MM/yyyy, HH:mm:ss") },
+    { text: 'Second note', time: format(new Date(), "dd/MM/yyyy, HH:mm:ss") }
+  ])
+  const [textAdd, setTextAdd] = useState('');
+  const [input, setInput] = useState({name: '', value: '', visible: false})
 
-  const handleChangeRoute = ({target: {value}}) => setCurrentRoute(value)
+  const handleSetText = ({target: {value}}) => setTextAdd(value);
+
+  const handleAddNote = () => {
+    setNotes(prevNotes => prevNotes.concat({ text: textAdd, time: format(new Date(), "dd/MM/yyyy, HH:mm:ss")}));
+    setTextAdd('')
+  };
+
+  const handleDelete = ({target: {value}}) => setNotes(prevNotes => prevNotes.filter(({text}) => text !== value ));
+
+  const handleEdit = ({target: {name}}) => {
+    name !== input.name && setInput({ name, value: '', visible: true })
+    if (input.value) {
+      setNotes(prevNotes => prevNotes.map((item) => item.text !== input.name ? item : {text: input.value, time: item.time}));
+    }
+  };
+
+  const handleEditText = ({ target: { value, name } }) => setInput({ name, value, visible: true })
 
   return (
     <div>
-      <p>{currentRoute}</p>
-      {routes.map(item => (
-        <div key={item}>
-          <input 
-            type='radio' 
-            name='routes' 
-            value={item} 
-            onChange={handleChangeRoute}
-          />
-          {item}
+      {notes.map((item, index) => (
+        <div key={item.text}>
+          <h1>Note {index + 1}</h1>
+          <p>{item.text}</p>
+          <p>Create time: {item.time}</p>
+          <button name={item.text} onClick={handleEdit} >Edit</button>
+          {item.text === input.name && input.visible 
+            && <input name={item.text} onChange={handleEditText}/>
+          }
+          <button value={item.text} onClick={handleDelete}>Delete</button>
         </div>
       ))}
+
+      <textarea value={textAdd} onChange={handleSetText}/>
+      <button onClick={handleAddNote}>Add</button>
     </div>
   );
 }
