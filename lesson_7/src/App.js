08446fe3544ebf119e8f3929522243e54c9d3ad1
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-17 14:26:24 +0300

    Lesson_7. Task 5

--- lesson_7/src/App.js
+++ lesson_7/src/App.js
@@ -1,24 +1,45 @@
 import {useState} from 'react';
 
 function App() {
-  const [users, setUsers] = useState([
-    {name: 'Коля', surname: 'Иванов', age: 30, visible: true},
-    {name: 'Вася', surname: 'Петров', age: 40, visible: true},
-    {name: 'Петя', surname: 'Сидоров', age: 50, visible: true},
-  ])
+  const [items, setItems] = useState(['David', 'Boghdan', 'Alex']);
+  const [currentText, setCurrentText] = useState({value: '', name: ''})
+  const [visibleList, setVisibleList] = useState([]);
 
-  const handleChangeVisible = ({target: {name}}) => {
-    setUsers(prevUsers => prevUsers.map(item => item.surname !== name ? item : 
-      { name: item.name, surname: item.surname, age: item.surname, visible: !item.visible}))
+  const handleClickAppearInput = ({ target }) => {
+    const name = target.getAttribute('name');
+    visibleList.find(el => el === name) 
+    ? setVisibleList(prevList => prevList.filter(item => item !== name)) 
+    : setVisibleList(prevList => [...prevList, name]);
+  }
+
+  const handleChangeText = ({target: {name, value}}) => setCurrentText({name, value});
+
+  const handleBlurInput = () => {
+    visibleList.find(el => el === currentText.name)
+      ? setVisibleList(prevList => prevList.filter(item => item !== currentText.name))
+      : setVisibleList(prevList => [...prevList, currentText.name]);
+    setItems(prevItems => prevItems.map(item => item !== currentText.name ? item : currentText.value));
   }
 
   return (
     <div>
-      {users.map(item => <div key={item.surname}>
-          <input type='checkbox' name={item.surname} checked={item.visible} onChange={handleChangeVisible}/>
-          {item.name} {item.visible && [item.surname, (item.age)].join(' ')}
-        </div>
-      )}
+      <ul>
+        {items.map(item => (
+        <li key={item} >
+            <p 
+              name={item} 
+              onClick={handleClickAppearInput}
+              style={{display: 'inline'}}
+            >{item}</p>
+          <input 
+            type='text' 
+            name={item}
+            onChange={handleChangeText}
+            onBlur={handleBlurInput}
+            style={{ display: visibleList.find(el => el === item) ? 'inline' : 'none' }}
+          />
+        </li>))}
+      </ul>
     </div>
   );
 }
