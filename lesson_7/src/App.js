80f0d1a73a3e5ab83ee9f9d40b26403696ae26a9
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-17 17:01:48 +0300

    Lesson_7. Task 10

--- lesson_7/src/App.js
+++ lesson_7/src/App.js
@@ -1,57 +1,38 @@
 import {useState} from 'react';
+import { format, eachDayOfInterval} from 'date-fns';
+import {uk} from 'date-fns/locale';
 
 function App() {
-  const [workers, setWorkers] = useState([
-    { name: 'Roberto', surname: 'Carlos', salary: 1000 },
-    { name: 'Dan', surname: 'Williams', salary: 2000 },
-    { name: 'Anna', surname: 'Gomes', salary: 4000 }
-  ])
+  const [daysOfWeek] = useState(eachDayOfInterval({
+    start: new Date(2021, 7, 16),
+    end: new Date(2021, 7, 22)
+  }));
+  const [formatedDays, setFormatedDays] = useState(daysOfWeek.map(item => format(item, 'EEEE')));
 
-  const handleSortForName = () => setWorkers(prevWorekers => [...prevWorekers.sort((a, b) => {
-    const nameA = a.name.toUpperCase();
-    const nameB = b.name.toUpperCase();
-    if (nameA < nameB) {
-      return -1;
+  const handleChangeLanguage = ({target: {value}}) => {
+    switch (value) {
+      case 'English':
+        setFormatedDays(daysOfWeek.map(item => format(item, 'EEEE')));
+        break;
+      case 'Ukrainian':
+        setFormatedDays(daysOfWeek.map(item => format(item, 'EEEE', {locale: uk})));
+        break;
+    
+      default:
+        break;
     }
-    if (nameA > nameB) {
-      return 1;
-    }
-    return 0;
-  })]);
-
-  const handleSortForSurname = () => setWorkers(prevWorekers => [...prevWorekers.sort((a, b) => {
-    const surnameA = a.surname.toUpperCase();
-    const surnameB = b.surname.toUpperCase();
-    if (surnameA < surnameB) {
-      return -1;
-    }
-    if (surnameA > surnameB) {
-      return 1;
-    }
-    return 0;
-  })]);
-
-  const handleSortForSalary = () => setWorkers(prevWorekers => [...prevWorekers.sort((a, b) => a.salary - b.salary)]);
-
+  }
 
   return (
     <div>
-      <table>
-        <tbody>
-          {workers.map(item => (
-            <tr>
-              <td>{item.name}</td>
-              <td>{item.surname}</td>
-              <td>{item.salary}</td>
-            </tr>
-          ))}
-          <tr>
-            <td><button onClick={handleSortForName}>Sort</button></td>
-            <td><button onClick={handleSortForSurname}>Sort</button></td>
-            <td><button onClick={handleSortForSalary}>Sort</button></td>
-          </tr>
-        </tbody>
-      </table>
+      <select onChange={handleChangeLanguage}>
+        <option>English</option>
+        <option>Ukrainian</option>
+      </select>
+        
+      <select>
+        {formatedDays.map(item => <option key={item}>{item}</option>)}
+      </select>
     </div>
   );
 }
