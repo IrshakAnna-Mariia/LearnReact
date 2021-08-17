bbf6095a47728ac8d247450fea57b557b8fa3c96
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-17 15:25:15 +0300

    Lesson_7. Task 7

--- lesson_7/src/App.js
+++ lesson_7/src/App.js
@@ -1,46 +1,29 @@
 import {useState} from 'react';
-import useStyles from './styles';
 
 function App() {
-  const [users, setUsers] = useState([
-    { name: 'Коля', age: 30 },
-    { name: 'Вася', age: 40 },
-    { name: 'Петя', age: 50 },
+  const [routes, setRoutes] = useState([
+    'Черкаси - Київ',
+    'Черкаси - Одеса',
+    'Черкасі - Лвів'
   ]);
-  const [visibleInputs, setVisibleInputs] = useState([]);
-  const [inputText, setInputText] = useState({name: '', value: ''})
+  const [currentRoute, setCurrentRoute] = useState('');
 
-  const classes = useStyles();
-
-  const handleClickEdit = ({target: {name}}) => {
-    if (visibleInputs.find(el => el === name)) {
-      console.log(inputText.name, name)
-      if (inputText.name === name){
-        setUsers(prevUsers => prevUsers.map(item => item.name !== name ? item : { name, age: inputText.value }))
-        setVisibleInputs(prevList => prevList.filter(item => item !== name))
-      }
-    } else setVisibleInputs(prevList => [...prevList, name]);
-  }
-
-  const handleChangeInputText = ({target: {name, value}}) => setInputText({name, value})
+  const handleChangeRoute = ({target: {value}}) => setCurrentRoute(value)
 
   return (
     <div>
-      <table>
-        <tbody>
-          {users.map(item => (
-            <tr key={item.name} >
-              <td className={classes.td}>{item.name}</td>
-              <td className={classes.td}>
-                {item.age}
-                <button name={item.name} onClick={handleClickEdit}>Редактировать</button>
-                {visibleInputs.find(el => el === item.name) 
-                && <input type='number' name={item.name} onChange={handleChangeInputText}/>}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <p>{currentRoute}</p>
+      {routes.map(item => (
+        <div key={item}>
+          <input 
+            type='radio' 
+            name='routes' 
+            value={item} 
+            onChange={handleChangeRoute}
+          />
+          {item}
+        </div>
+      ))}
     </div>
   );
 }
