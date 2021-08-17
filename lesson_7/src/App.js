511b04cd86613e94a3394887a380a335b3038a87
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-17 11:46:05 +0300

    Lesson_7. Task 1

--- lesson_7/src/App.js
+++ lesson_7/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [names, setNames] = useState([
+    {name: 'David', checked: false}, 
+    {name: 'Alex', checked: false},
+    {name: 'Bodya', checked: false}
+  ]);
+
+  const handleChengeDecoration = ({target: {name}}) => setNames(prevNames => {
+    return prevNames.map(item => name !== item.name ? item : {name, checked: !item.checked})
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {names.map(item => (
+          <li 
+            key={item.name}
+            style={{textDecoration: item.checked ? 'line-through': 'none'}} 
+          >
+            <input name={item.name} onChange={handleChengeDecoration} type='checkbox'/>
+            {item.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
