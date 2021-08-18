98d7996f58b8c73726b062f4794d707edeabcdb0
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-18 11:44:03 +0300

    Lesson_7. Task 14

--- lesson_7/src/App.js
+++ lesson_7/src/App.js
@@ -1,9 +1,27 @@
 import {useState} from 'react';
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [city, setCity] = useState('');
+  const [selectedCity, setSelectedCity]= useState('');
+
+  const handleClikAddCity = () => {
+    setCities(prevCities => city ? prevCities.concat(city) : prevCities);
+    setCity('');
+  }
+
+  const handleChangeCity = ({target: {value}}) => setCity(value);
+
+  const handleSetCity = ({ target: { value } }) => setSelectedCity(value);
 
   return (
     <div>
+      <input value={city} onChange={handleChangeCity}/>
+      <button onClick={handleClikAddCity}>Add City</button>
+      <select value={selectedCity} onChange={handleSetCity}>
+        {cities.map(item => <option key={item} value={item}>{item}</option>)}
+      </select>
+      <p>{selectedCity}</p>
     </div>
   );
 }
