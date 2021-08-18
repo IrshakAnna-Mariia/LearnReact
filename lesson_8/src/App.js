30f8826c8bad9113817debaec9c9d2ca635cddfa
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-18 14:36:01 +0300

    Lesson_8. Task 1

--- lesson_8/src/App.js
+++ lesson_8/src/App.js
@@ -1,8 +1,31 @@
 import {useState} from 'react';
+import TableTr from './components/TableTr';
 
 function App() {
+  const [users, setUsers] = useState([
+    { name: 'David', surname: 'Dubliakov', age: 18 },
+    { name: 'Alex', surname: 'Kostenko', age: 22 },
+    { name: 'Bodya', surname: 'Zavgorodskiy', age: 22 },
+  ])
+
   return (
     <div className="App">
+      <table>
+        <thead>
+          <tr>
+            <th> Name </th>
+            <th> Surname </th>
+            <th> Age </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map(user => (
+              <TableTr name={user.name} surname={user.surname} age={user.age}/>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ lesson_8/src/components/TableTr.js
@@ -0,0 +1,11 @@
+const TableTr = ({name, surname, age}) => {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{surname}</td>
+      <td>{age}</td>
+    </tr>
+  )
+}
+
+export default TableTr;
