fafdcc5ae3114258eafd16229ddf2c6dd6bd2601
IrshakAnna-Mariia <irshakam@gmail.com>
2021-08-19 09:19:30 +0300

    Lesson_10. Task 1. Initial commit

---
+++ lesson_10/task1/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Button } from 'antd'
+
+const App = () => {
+
+
+
+  return (
+    <div className="App">
+        <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
